73ed15b184283856434d32d6db278f57152ec6d7
Brittany Nicole <bnsquires1@gmail.com>
2024-04-14 22:03:10 -0400

    updates to hw

---
+++ controlflow.js
@@ -0,0 +1,28 @@
+// The area in which the plants are contained is circular, with a radius of 5 meters.
+// The formula for calculating the area of a circle is PI multiplied by the radius, squared:
+//const PI = 3.1415;
+//const area = PI * radius * radius;
+// Each plant requires a minimum space of 0.8 square meters.
+// The area is starting with 20 plants.
+// The plants double in number every week.
+
+// predict the plant growth after a specific number of weeks
+
+totalRadius = 5;
+const PI = 3.14;
+let space = 0.8;
+let start = 20 ;
+
+let area = 5 * 5 * PI; {
+console.log(area)
+}
+// 78.54 meters
+
+const plantAreaWeek1= space * start;
+
+
+
+// (20 * 2) ++1 for 4 loops
+
+
+// 98 total trees < total
