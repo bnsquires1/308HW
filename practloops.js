779fbcfba7ba024a85f38c61a6970da7e1bee422
Brittany Nicole <bnsquires1@gmail.com>
2024-04-14 22:56:53 -0400

    fizzbuzz

--- practloops.js
+++ practloops.js
@@ -1,3 +1,21 @@
+// Loop through all numbers from 1 to 100.
+
 for (let i =1; i <=100; i++){
     console.log(i)
+}
+
+// If a number is divisible by 3, log “Fizz.”
+
+let fizz = "fizz"
+for (let i = 3; i <= 100; i++) {
+    if (i % 3 === 0)
+    console.log(fizz)
+}
+
+// If a number is divisible by 5, log “Buzz.”
+
+let buzz = "buzz"
+for (let i = 5; i <= 100; i++){
+    if (i % 5 === 0)
+    console.log(buzz)
 }
